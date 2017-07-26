84556d0ff46780e13c7997a732549857efa55cf7
Kararin <charms_devil@mail.ru>
2017-07-26 10:15:55 +0300

    Subject learned

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ const buttonEvents = () => {
         .map(e => e.clientX)
         .scan((count, clientX) => count + clientX, 0) // works as reduce
         .subscribe((count) => console.log(`clicked ${count}`));
-}
+};
 
-
-document.addEventListener('DOMContentLoaded', () => {
+const observable = () => {
     let observable = Rx.Observable.create((observer) => {
             let id = setInterval(() => {
                 observer.next(4);
@@ -36,4 +35,55 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('un');
         subscription.unsubscribe();
     }, 3000);
+};
+
+const observableUnicast = () => {
+    var obs = Rx.Observable.create((observer) => {
+        let i = 0;
+
+        setInterval(() => {
+            observer.next(i++)
+        }, 1000);
+    });
+
+    obs.subscribe({
+        next: (value) => {
+            console.log(`1 - ${value}`);
+        }
+    });
+
+    setTimeout(() => {
+        obs.subscribe({
+            next: (value) => {
+                console.log(`2 - ${value}`);
+            }
+        });
+    }, 3000);
+};
+
+const subjectMulty = () => {
+    var sub = new Rx.Subject(),
+        i = 0;
+
+    setInterval(() => {
+        sub.next(i++);
+    }, 1000);
+
+    sub.subscribe({
+        next: (value) => {
+            console.log(`1 - ${value}`);
+        }
+    });
+
+    setTimeout(() => {
+        sub.subscribe({
+            next: (value) => {
+                console.log(`2 - ${value}`);
+            }
+        });
+    }, 3000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    subjectMulty();
 });
